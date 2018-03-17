c6d65110f02d6846cb87b3c11a5beb8e43a9b545
Mark Phillips <mark.phillips@trizetto.com>
2018-03-16 18:09:39 -0700

    added
    added

---
+++ hostGmsaSetup.ps1
@@ -0,0 +1,11 @@
+install-windowsfeature AD-Domain-Services
+Import-Module ADDSDeployment
+
+Install-AdServiceAccount EU-SVC
+Install-AdServiceAccount DH-SVC
+
+
+Start-BitsTransfer https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/live/windows-server-container-tools/ServiceAccounts/CredentialSpec.psm1
+Import-Module ./CredentialSpec.psm1
+New-CredentialSpec -Name EU-SVC  -AccountName EU-SVC
+New-CredentialSpec -Name DH-SVC  -AccountName DH-SVC
